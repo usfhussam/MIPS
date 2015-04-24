852dd0b118d00aa83bebc2cd71ac241dd42a6ca2
Youssef Hossam <usfhussam@live.com>
2015-04-24 20:34:31 +0300

    Added ArithmeticLogicUnit

--- src/instructions/Instruction.java
+++ src/instructions/Instruction.java
@@ -24,16 +24,17 @@ public class Instruction {
 		adjustFormat(firstHalf);
 		String[] registers = secondHalf.trim().split(",");
 		switch(format) {
-		case 0: rd = RegisterFile.getRegister(registers[0].trim());
-		if(shift) {
-			rt = RegisterFile.getRegister(registers[1].trim());
-			shamt = Integer.parseInt(registers[2].trim());
-		}
-		else{
-			rs = RegisterFile.getRegister(registers[1].trim());
-			rt = RegisterFile.getRegister(registers[2].trim());
-		}
-		break;
+		case 0: 
+			rd = RegisterFile.getRegister(registers[0].trim());
+			if(shift) {
+				rt = RegisterFile.getRegister(registers[1].trim());
+				shamt = Integer.parseInt(registers[2].trim());
+			}
+			else{
+				rs = RegisterFile.getRegister(registers[1].trim());
+				rt = RegisterFile.getRegister(registers[2].trim());
+			}
+			break;
 		case 1: 
 			rt = RegisterFile.getRegister(registers[0].trim());
 			rs = RegisterFile.getRegister(registers[1].trim());
@@ -52,49 +53,49 @@ public class Instruction {
 	public void adjustFormat(String methodName) {
 		type = methodName;
 		switch(methodName) {
-		case "add": 
-		case "sub":
-		case "and":
-		case "nor":
-		case "slt":
-		case "sltu":
-		
-			format = 0;
+			case "add": 
+			case "sub":
+			case "and":
+			case "nor":
+			case "slt":
+			case "sltu":
 			
-			break;
-		case "sll":
-		case "srl":
-			format = 0;
-			shift = true;
-			break;
-		case "addi":
-
-			format = 1;
-			break;
-		case "lui":
-		case "lw":
-		case "lb":
-		case "lbu":
-			format = 2;
-			break;
-		case "sw":
-		case "sb":
-			format = 3;
-			break;
-		case "beq":
-		case "bneq":
-			format = 4;
-			break;
-		case "j":
-		case "jal":
-			format = 5;
-			break;
-		case "jr":
-			format = 6; 
-			break;
-		default:
-			format = -1;
-			break;
+				format = 0;
+				
+				break;
+			case "sll":
+			case "srl":
+				format = 0;
+				shift = true;
+				break;
+			case "addi":
+	
+				format = 1;
+				break;
+			case "lui":
+			case "lw":
+			case "lb":
+			case "lbu":
+				format = 2;
+				break;
+			case "sw":
+			case "sb":
+				format = 3;
+				break;
+			case "beq":
+			case "bneq":
+				format = 4;
+				break;
+			case "j":
+			case "jal":
+				format = 5;
+				break;
+			case "jr":
+				format = 6; 
+				break;
+			default:
+				format = -1;
+				break;
 
 		}
 	}
@@ -117,6 +118,25 @@ public class Instruction {
 	public String getType() {
 		return type;
 	}
-
+	
+	public Register getRd() {
+		return rd;
+	}
+	
+	public Register getRt() {
+		return rt;
+	}
+	
+	public Register getRs() {
+		return rs;
+	}
+	
+	public int getShiftAmount() {
+		return shamt;
+	}
+	
+	public int getImmediateConstant() {
+		return iConstant;
+	}
 
 }
